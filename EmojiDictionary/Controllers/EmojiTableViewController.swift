1c1a735edaa34385b90a7175c714d8295302a772
Ronny Abraham <this.ronny@gmail.com>
2017-11-29 13:25:33 +0200

    get rid of delete indicator

--- EmojiDictionary/Controllers/EmojiTableViewController.swift
+++ EmojiDictionary/Controllers/EmojiTableViewController.swift
@@ -98,6 +98,9 @@ class EmojiTableViewController: UITableViewController {
         tableView.reloadData()
     }
  
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCellEditingStyle {
+        return .none
+    }
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
