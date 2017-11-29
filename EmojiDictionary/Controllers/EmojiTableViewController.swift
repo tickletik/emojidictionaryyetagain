617fd7b7582d0a145216703be99e7297c35bee9f
Ronny Abraham <this.ronny@gmail.com>
2017-11-29 12:14:06 +0200

    error checking on # rows

--- EmojiDictionary/Controllers/EmojiTableViewController.swift
+++ EmojiDictionary/Controllers/EmojiTableViewController.swift
@@ -44,13 +44,15 @@ class EmojiTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return emojis.count
+        if section == 0 {
+            return emojis.count
+        } else {
+            return 0
+        }
     }
 
     /*
