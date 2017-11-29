e2bebfd36c5edb092fe857387cb944e9ee52ef6a
Ronny Abraham <this.ronny@gmail.com>
2017-11-29 13:18:48 +0200

    add editing bar button item with functionality

--- EmojiDictionary/Controllers/EmojiTableViewController.swift
+++ EmojiDictionary/Controllers/EmojiTableViewController.swift
@@ -81,6 +81,13 @@ class EmojiTableViewController: UITableViewController {
         
         print("\(emoji.symbol) \(indexPath)")
     }
+    
+    @IBAction func editButtonTapped(_ sender: UIBarButtonItem) {
+        let tableViewEditingMode = tableView.isEditing
+        
+        tableView.setEditing(!tableViewEditingMode, animated: true)
+    }
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
