6a6e9a46d412e9101622ccce22a1dd9cf56de409
Ronny Abraham <this.ronny@gmail.com>
2017-11-30 15:45:29 +0200

    make meatballs

--- EmojiDictionary/Controllers/EmojiTableViewCell.swift
+++ EmojiDictionary/Controllers/EmojiTableViewCell.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class EmojiTableViewCell: UITableViewCell {
-
+    
+    
+    @IBOutlet weak var symbolLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
