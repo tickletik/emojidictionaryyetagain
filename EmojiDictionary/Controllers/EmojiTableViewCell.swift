c864b6af48bff9c46ff8577c6f7b4b78eae3adcd
Ronny Abraham <this.ronny@gmail.com>
2017-11-30 15:08:55 +0200

    add new EmojiTableCell.swift

---
+++ EmojiDictionary/Controllers/EmojiTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EmojiTableViewCell.swift
+//  EmojiDictionary
+//
+//  Created by ronny abraham on 11/30/17.
+//  Copyright © 2017 ronny abraham. All rights reserved.
+//
+
+import UIKit
+
+class EmojiTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
