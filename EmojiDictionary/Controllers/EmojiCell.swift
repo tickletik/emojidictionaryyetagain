2b7e4d0e71e227e8d2bbd79739b1cf21d4efdd92
Ronny Abraham <this.ronny@gmail.com>
2017-11-30 12:24:00 +0200

    create custom EmojiCell cocoa subclass, rename labels

---
+++ EmojiDictionary/Controllers/EmojiCell.swift
@@ -0,0 +1,24 @@
+//
+//  EmojiCell.swift
+//  EmojiDictionary
+//
+//  Created by ronny abraham on 11/30/17.
+//  Copyright © 2017 ronny abraham. All rights reserved.
+//
+
+import UIKit
+
+class EmojiCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
